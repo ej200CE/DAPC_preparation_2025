27a0144822b816522db106109db646698014fe08
ej200CE <ruban.km73@gmail.com>
2025-08-15 16:09:25 +0200

    gries-misra

---
+++ self_tries/Gries_Misra.py
@@ -0,0 +1,17 @@
+
+def gries_misra(n):
+    factor = [0]*n
+    primes = []
+    for x in range(2, n):
+        if not factor[x]:
+            factor[x] = x
+            primes.append(x)
+        for p in primes:
+            if p > factor[x] or p*x >= n:
+                break
+            factor[p*x] = p
+    return factor, primes
+
+fac, prm = gries_misra(30)
+print(fac)
+print(prm)
